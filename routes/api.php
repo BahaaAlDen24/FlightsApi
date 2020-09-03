95a0a45a8cc7f54630dba4eeddae1e0e892486ae
Bahaa Aldeen <Bahaa Aldeen@DESKTOP-5GJDND0>
2020-09-04 02:48:52 +0300

    Controllers with auth2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,26 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@login');
+
+Route::middleware('auth:api')->group(function() {
+    Route::resource('Airline', 'AirlinesController');
+    Route::resource('Airplane', 'AirplanesController');
+    Route::resource('Airport', 'AirportsController');
+    Route::resource('BankAccount', 'BankAccountsController');
+    Route::resource('Bank', 'BankController');
+    Route::resource('BookedFlight', 'BookedFlightsController');
+    Route::resource('CanceledFlight', 'CanceledFlightsController');
+    Route::resource('City', 'CitiesController');
+    Route::resource('Country', 'CountriesController');
+    Route::resource('FlightOffer', 'FlightOffersController');
+    Route::resource('Flight', 'FlightsController');
+    Route::resource('FlightType', 'FlightTypesController');
+    Route::resource('Hotel', 'HotelsController');
+    Route::resource('Offer', 'OffersController');
+    Route::resource('UserProfile', 'UserProfileController');
+    Route::resource('User', 'UsersController');
 });
